ac7c0fe2895d0284ed57df0671494d668849b9ac
Max Walley <58304135+maxwalley@users.noreply.github.com>
2024-06-12 14:27:46 +0100

    Updated the interface of the delay class

---
+++ include/AudioDelayEngine/Delay.h
@@ -0,0 +1,24 @@
+#include <concepts>
+#include "CircularBuffer.h"
+
+template<std::floating_point FloatType = float>
+class Delay
+{
+public:
+    Delay(std::chrono::seconds maxDelayTime)  : maxDelay(maxDelayTime)
+    ~Delay();
+
+    void prepare(int sampleRate, int bufferSize)
+    {
+        buffer = std::make_unique<CircularBuffer<FloatType>>(sampleRate, bufferSize, maxDelay);
+    }
+
+    void process(std::span<FloatType> buffer)
+    {
+        
+    }
+
+private:
+    std::chrono::seconds maxDelay;
+    std::unique_ptr<CircularBuffer<FloatType>> buffer = nullptr;
+};
