37e842a164e5060bb242358f4ead083212078ce1
Max Walley <58304135+maxwalley@users.noreply.github.com>
2024-07-03 15:52:09 +0100

    Added the CAS exchange loop for the delay line list

--- include/AudioDelayEngine/Delay.h
+++ include/AudioDelayEngine/Delay.h
@@ -2,6 +2,7 @@
 
 #include <concepts>
 #include <algorithm>
+#include <cassert>
 #include "CircularBuffer.h"
 #include "DelayLine.h"
 
@@ -9,27 +10,71 @@ template<std::floating_point FloatType = float>
 class Delay
 {
 public:
-    Delay(std::chrono::seconds maxDelayTime)  : maxDelay(maxDelayTime) {}
+    Delay(std::chrono::seconds maxDelayTime)  : maxDelay(maxDelayTime) 
+    {
+        lines = std::make_unique<LineList>();
+        safeLines = lines.get();
+    }
+
     ~Delay() {}
 
     void addLine(std::chrono::milliseconds lineDelayTime)
     {
+        std::unique_ptr<LineList> newLinesList = std::make_unique<LineList>();
 
+        //Copy all lines across from main lines list
+        std::copy(lines.begin(), lines.end(), std::back_inserter(newLinesList));
+
+        //Create new line and add to new lines list
+        std::shared_ptr<DelayLine<FloatType>> newLine = std::make_shared<DelayLine<FloatType>>(lineDelayTime);
+
+        newLinesList->push_back(newLine);
+
+        //Wait for safe line list pointer to be out of real-time use and replace with the new list
+        for(LineList* expected = lines.get(); !safeLines.compare_exchange_strong(expected, newLinesList.get()); expected = lines.get());
+
+        //Replace main lines list with new list
+        lines = std::move(newLinesList);
+    }
+
+    void removeLine(size_t lineIndex)
+    {
+        if(lineIndex >= lines.size())
+        {
+            return;
+        }
+
+        std::unique_ptr<LineList> newLinesList = std::make_unique<LineList>();
+
+        //Copy all lines across from main lines list
+        std::copy(lines.begin(), lines.end(), std::back_inserter(newLinesList));
+
+        //Remove line to delete from new list
+        newLinesList->erase(lines.begin() + lineIndex);
+
+        //Wait for safe line list pointer to be out of real-time use and replace with the new list
+        for(LineList* expected = lines.get(); !safeLines.compare_exchange_strong(expected, newLinesList.get()); expected = lines.get());
+
+        //Replace main lines list with new list
+        lines = std::move(newLinesList);
     }
 
     DelayLine<FloatType>* getLine(size_t lineIndex)
     {
-
+        //We do not have to worry about thread safety here since this thread is the only one which can edit the lines list
+        return lineIndex >= lines.size() ? nullptr : lines[lineIndex];
     }
 
     const DelayLine<FloatType>* getLine(size_t lineIndex) const
     {
-
+        //We do not have to worry about thread safety here since this thread is the only one which can edit the lines list
+        return lineIndex >= lines.size() ? nullptr : lines[lineIndex];
     }
 
     size_t numLines() const
     {
-
+        //We do not have to worry about thread safety here since this thread is the only one which can edit the lines list
+        return lines.size();
     }
 
     void prepare(int sampleRate, int bufferSize)
@@ -44,12 +89,25 @@ public:
 
     void process(std::span<FloatType> data)
     {
+        //CAS loop failed
+        if(safeLines == nullptr)
+        {
+            assert(false);
+            return;
+        }
 
+        //Mark that lines are now in use by real-time thread
+        LineList* realtimeLines = safeLines.exchange(nullptr);
+
+        //Mark that lines are now safe to edit
+        safeLines = realtimeLines;
     }
 
 private:
     std::chrono::seconds maxDelay;
     std::unique_ptr<CircularBuffer<FloatType>> buffer = nullptr;
 
-    std::vector<DelayLine<FloatType>> lines;
+    using LineList = std::vector<std::shared_ptr<DelayLine<FloatType>>>;
+    std::unique_ptr<LineList> lines;
+    std::atomic<LineList*> safeLines;
 };
